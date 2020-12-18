80ed6710379fb0e60ee5cd560c5cb1a338c88853
senhorsolar <25336720+senhorsolar@users.noreply.github.com>
2020-12-18 02:20:12 -0500

    iq twist can now parse cli type strings

---
+++ include/iq_twist.h
@@ -0,0 +1,381 @@
+#include <complex>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <wordexp.h>
+
+#include "../dlx/dlx.h"
+
+using namespace std;
+
+
+class IqTwist : public DancingLinks {
+public:	
+
+	typedef vector<vector<complex<int> > > piece;
+	typedef pair<int, int> coord;
+	typedef pair<char, coord> peg;
+	
+	IqTwist(string cmd) : IqTwist(parse_cmd(cmd)) {}
+	IqTwist(vector<peg> pegs) : DancingLinks(iq_twist_dlx_matrix(pegs),
+						 iq_twist_column_names(pegs))
+		{
+		}
+
+	vector<peg> parse_cmd(string cmd) {
+
+		vector<peg> pegs;
+		
+		set<coord> peg_placements;
+		map<char, int> possible_placements = {
+					{'r', 1},
+					{'b', 2},
+					{'y', 2},
+					{'g', 2}
+		};
+
+		cmd = cmd.insert(0, "dummy_cmd ");
+
+		wordexp_t p;
+		::wordexp(cmd.c_str(), &p, WRDE_NOCMD);
+
+		char c;
+		while ((c = getopt(p.we_wordc, p.we_wordv, "r:b:y:g:")) != -1) {
+			
+			switch(c) {
+			case 'r':
+			case 'b':
+			case 'y':
+			case 'g':
+				// TODO - come uop with error msg
+				if (strlen(optarg) != 2) {}
+					
+				
+				int coord_1 = char_to_int(optarg[0]);
+				int coord_2 = char_to_int(optarg[1]);
+				
+				// TODO - come up with error msg
+				if ((coord_1 < 0) or (coord_2 < 0)) {}
+				if ((coord_1 >= 4) or (coord_2 >= 8)) {}
+				
+				coord po = {coord_1, coord_2};
+				
+				// TODO - come up with error msg
+				if (peg_placements.count(po) or !(possible_placements[c]--)) {}
+				else
+					peg_placements.insert(po);
+				
+				pegs.push_back({c, po});
+				break;
+			}
+		}
+
+		return pegs;
+	}
+	
+	piece rotate_piece(piece p) {
+		int nrows = p.size();
+		int ncols = p[0].size();
+		
+		piece rp; // rotated piece
+		
+		for (int j = ncols-1; j >= 0; j--) {
+			vector<complex<int> > row;
+			for (int i = 0; i < nrows; i++) {
+				row.push_back(p[i][j]);
+			}
+			rp.push_back(row);
+		}
+		
+		return rp;
+	}
+
+	piece mirror_piece(piece p) {
+		int nrows = p.size();
+		int ncols = p[0].size();
+		
+		piece mp; // mirrored piece
+		
+		for (int i = 0; i < nrows; i++) {
+			vector<complex<int> > row; 
+			for (int j = ncols-1; j >= 0; j--) {
+				row.push_back(p[i][j]);
+			}
+			mp.push_back(row);
+		}
+		
+		return mp;
+	}
+
+	string piece_to_str(piece p) {
+		ostringstream oss;
+		
+		int nrows = p.size();
+		int ncols = p[0].size();
+		
+		for (int i = 0; i < nrows; i++) {
+			for (int j = 0; j < ncols; j++) {
+				oss << p[i][j].real();
+				oss << ((j < ncols-1) ? "," : "\n");
+			}
+		}
+		
+		return oss.str();
+	}
+
+	void merge_imag(piece& p1, piece p2) {
+		// Assumes pieces are of the same dimension
+		int nrows = p1.size();
+		int ncols = p1[0].size();
+		
+		for (int i = 0; i < nrows; i++) {
+			for (int j = 0; j < ncols; j++) {
+				p1[i][j].imag(p1[i][j].imag() | p2[i][j].imag());
+			}
+		}
+	}
+	
+	vector<piece> unique_configurations(piece p) {
+		
+		vector<piece> rotations;
+		
+		rotations.push_back(p);
+		for (int i = 0; i < 3; i++)
+			rotations.push_back(rotate_piece(rotations.back()));
+		
+		rotations.push_back(mirror_piece(p));
+		for (int i = 0; i < 3; i++)
+			rotations.push_back(rotate_piece(rotations.back()));
+		
+		map<string, piece> uc; // unique configurations
+		
+		for (piece it : rotations) {
+			string key = piece_to_str(it);
+			
+			if (uc.count(key)) {
+				merge_imag(uc[key], it);
+			} else {
+				uc[key] = it;
+			}
+		}
+		
+		vector<piece> unique_pieces;
+		for (auto& kv : uc) {
+			unique_pieces.push_back(kv.second);
+		}
+		
+		return unique_pieces;
+	}
+
+	int real_sum(piece p) {
+		int sum = 0;
+		
+		for (int i = 0; i < p.size(); i++)
+			for (int j = 0; j < p[0].size(); j++)
+				sum += p[i][j].real();
+		
+		return sum;
+	}
+	
+	vector<vector<complex<int> > > init_complex_row() {
+		vector<vector<complex<int> > > row2d;
+		
+		for (int i = 0; i < iq_twist_board.size(); i++) {
+			vector<complex<int> > row(iq_twist_board[0].size(), 0);
+			row2d.push_back(row);
+		}
+		
+		return row2d;
+	}
+
+	vector<string> iq_twist_column_names(vector<peg> pegs) {
+		vector<string> column_names = {"r1", "r2", "b1", "b2", "y1", "y2", "g1", "g2"};
+		
+		for (int i = 0; i < iq_twist_board.size(); i++) {
+			for (int j = 0; j < iq_twist_board[0].size(); j++) {
+				string s = to_string(i) + to_string(j);
+				column_names.push_back(s);
+			}
+		}
+		
+		for (peg p : pegs) {
+			auto [r_coord, c_coord] = p.second;
+			
+			string s = "p" + to_string(r_coord) + to_string(c_coord);
+			column_names.push_back(s);
+		}
+		
+		return column_names;
+	}
+
+	vector<vector<int> > iq_twist_dlx_matrix(vector<peg> pegs) {
+		
+		int r = iq_twist_board.size();
+		int c = iq_twist_board[0].size();
+		
+		vector<vector<int> > mat;
+
+		int debug_idx = 0;
+		for (int piece_idx = 0; piece_idx < pieces.size(); piece_idx++) {
+			piece p = pieces[piece_idx];
+			for (piece uc : unique_configurations(p)) {
+				
+				int pr = uc.size();
+				int pc = uc[0].size();
+
+				for (int i = 0; i <= (r-pr); i++) {
+					for (int j = 0; j <= (c-pc); j++) {
+						
+						vector<vector<complex<int> > > row2d = init_complex_row();
+						
+						for (int ii = 0; ii < pr; ii++)
+							for (int jj = 0; jj < pc; jj++)
+								row2d[i+ii][j+jj] = uc[ii][jj];
+
+						vector<int> row(pieces.size());
+						row[piece_idx] = 1;
+						
+						for (vector<complex<int> > row1d : row2d)
+							for (complex<int> val : row1d)
+								row.push_back(val.real());
+						
+						for (peg p_ : pegs) {
+							int can_place_peg = 0;
+							
+							coord piece_idxs = color_to_idxs.at(p_.first);
+							
+							if ((piece_idxs.first == piece_idx) or (piece_idxs.second == piece_idx)) {
+								int r_coord = p_.second.first;
+								int c_coord = p_.second.second;
+								if (row2d[r_coord][c_coord].imag() == 1)
+									can_place_peg = 1;
+							}
+							
+							row.push_back(can_place_peg);
+							
+							
+						}
+						
+						mat.push_back(row);
+						debug_idx++;
+					}
+				}
+			}
+		}
+		
+		return mat;
+	}
+	
+	int char_to_int(char c) {
+		return c - '0';
+	}
+
+	void add_coord(vector<coord> &coords, string s) {
+		pair<int, int> p{char_to_int(s[0]), char_to_int(s[1])};
+		coords.push_back(p);
+	}
+
+	char assign_color(string s) {
+		int d = char_to_int(s[1]) - 1;
+		
+		char color = s[0];
+		if (d)
+			color = toupper(color);
+		
+		return color;
+	}
+
+protected:
+	
+	void display_solution(int q) {
+		char board[32];
+		for (int k = 0; k < q; k++) {
+			vector<coord> coords;
+			char color;
+
+			Node* j_node = solution[k];
+
+			string s = j_node->column->name;
+
+			if (isdigit(s[0]))
+				add_coord(coords, s);
+			else if (s.size() == 2)
+				color = assign_color(s);
+
+			for (Node* i_node = j_node->right; i_node != j_node; i_node = i_node->right) {
+				string s = i_node->column->name;
+
+				if (isdigit(s[0]))
+					add_coord(coords, s);
+				else if (s.size() == 2)
+					color = assign_color(s);
+			}
+
+			for (auto& [r, c] : coords) {
+				board[r*8 + c] = color;
+			}
+		}
+
+		for (int i = 0; i < 32; i++)
+			cout << board[i];
+		cout << '\n';
+	}
+	
+private:
+        static const vector<piece> pieces;
+	static const map<char, pair<int, int> > color_to_idxs;
+	static const vector<vector<int> > iq_twist_board;
+};
+
+const vector<vector<int> > IqTwist::iq_twist_board = {
+	{1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+const map<char, IqTwist::coord> IqTwist::color_to_idxs = {
+	{'r', {0, 1}},
+	{'b', {2, 3}},
+	{'y', {4, 5}},
+	{'g', {6, 7}}
+};
+
+
+const complex<int> P(1, 1); // Peg opening
+const complex<int> X(1, 0); // Regular element
+const complex<int> O(0, 0); // Empty element
+
+const vector<IqTwist::piece> IqTwist::pieces = {
+	
+	// Red pieces
+	{{X, P, O},
+	 {O, X, X}},
+	
+	{{P, X, P},
+	 {O, O, X}},
+	
+	// Blue pieces
+	{{X, X, X},
+	 {O, P, P}},
+	
+	{{X, X, P, X}},
+	
+	// Yellow pieces
+	{{P, O, O},
+	 {P, X, X},
+	 {O, P, O}},
+	
+	{{P, X, X}},
+	
+	// Green pieces
+	{{P, P},
+	 {O, X}},
+	
+	{{P, X, X},
+	 {O, P, O}}
+};
---
+++ test.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <wordexp.h>
+
+#include "include/iq_twist.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	string cmd = "poo -r12";
+	IqTwist iqtwist = IqTwist(cmd);
+	iqtwist.search();
+
+	return 0;
+}
